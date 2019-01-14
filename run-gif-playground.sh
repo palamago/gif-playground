28a07e5aa7141c67c132bc5ed5696cf97e886fb6
Pablo H. Paladino <paladinop@gmail.com>
2019-01-14 18:25:57 -0500

    fix path

--- run-gif-playground.sh
+++ run-gif-playground.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 if ! pgrep -fl "run-gif-playground.sh" > /dev/null
 then
-    echo "Running !"
-else
-    python gif_bot.py
+    	echo "Running !"
+else	
+	. .env
+	python gif_bot.py
 fi
